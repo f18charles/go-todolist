e31b25a29d5f484dd2e7d0f00739498e2d1893d6
Favor Charles Owuor <133737127+f18charles@users.noreply.github.com>
2026-03-09 12:38:07 +0300

    Update todo_service.go

--- internal/internal/service/todo_service.go
+++ internal/internal/service/todo_service.go
@@ -1,1 +1,49 @@
+package service
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"todolist/internal/models"
+	"todolist/internal/repository"
+)
+
+type TodoService struct {
+	Repo repository.TodoRepository
+}
+
+func NewTodoService(r repository.TodoRepository) *TodoService {
+	return &TodoService{Repo: r}
+}
+
+func (s *TodoService) Create(title string) (models.Todo, error) {
+	todo := models.Todo{
+		ID:        uuid.New().String(),
+		Title:     title,
+		Completed: false,
+		CreatedAt: time.Now(),
+	}
+
+	err := s.Repo.Create(todo)
+
+	return todo, err
+}
+
+func (s *TodoService) List() ([]models.Todo, error) {
+	return s.Repo.GetAll()
+}
+
+func (s *TodoService) Complete(id string) error {
+	todo, err := s.Repo.GetByID(id)
+	if err != nil {
+		return err
+	}
+
+	todo.Completed = true
+
+	return s.Repo.Update(todo)
+}
+
+func (s *TodoService) Delete(id string) error {
+	return s.Repo.Delete(id)
+}
