65278322b4748aed4ec987184d258df63e54c923
Favor Charles Owuor <133737127+f18charles@users.noreply.github.com>
2026-03-09 12:40:54 +0300

    Update chi_router.go

--- internal/router/chi_router.go
+++ internal/router/chi_router.go
@@ -6,11 +6,12 @@ import (
 )
 
 func NewChiRouter(h *handler.ChiHandler) *chi.Mux {
-
 	r := chi.NewRouter()
 
 	r.Get("/todos", h.List)
 	r.Post("/todos", h.Create)
+	r.Patch("/todos/{id}", h.Complete)
+	r.Delete("/todos/{id}", h.Delete)
 
 	return r
 }
