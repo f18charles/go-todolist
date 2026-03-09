da7282070176069b70473005aa6711b9f8342664
Favor Charles Owuor <133737127+f18charles@users.noreply.github.com>
2026-03-09 12:40:34 +0300

    Update gin_router.go

--- internal/router/gin_router.go
+++ internal/router/gin_router.go
@@ -6,11 +6,12 @@ import (
 )
 
 func NewGinRouter(h *handler.GinHandler) *gin.Engine {
-
 	r := gin.Default()
 
 	r.GET("/todos", h.List)
 	r.POST("/todos", h.Create)
+	r.PATCH("/todos/:id", h.Complete)
+	r.DELETE("/todos/:id", h.Delete)
 
 	return r
 }
