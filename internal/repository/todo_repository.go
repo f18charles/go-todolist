e56549259e9989983a893037a9125a162c39a586
Favor Charles Owuor <133737127+f18charles@users.noreply.github.com>
2026-03-09 12:24:27 +0300

    Add TodoRepository interface for todo operations

---
+++ internal/repository/todo_repository.go
@@ -0,0 +1,11 @@
+package repository
+
+import "todolist/internal/models"
+
+type TodoRepository interface {
+	Create(todo models.Todo) error
+	GetByID(id string) (models.Todo, error)
+	GetAll() ([]models.Todo, error)
+	Update(todo models.Todo) error
+	Delete(id string) error
+}
