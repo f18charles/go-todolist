cbdfe08aa007e7639ea48c8b88fb6c7e080c1f92
Favor Charles Owuor <133737127+f18charles@users.noreply.github.com>
2026-03-09 12:29:15 +0300

    Create main.go

---
+++ cmd/server/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
+
+	"todolist/internal/handler"
+	"todolist/internal/repository"
+	"todolist/internal/router"
+	"todolist/internal/service"
+)
+
+func main() {
+
+	db, err := sql.Open("sqlite3", "todos.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repo := repository.NewSQLiteRepo(db)
+
+	service := service.NewTodoService(repo)
+
+	handler := handler.NewStdHandler(service)
+
+	r := router.NewStdRouter(handler)
+
+	log.Println("server running on :8080")
+
+	http.ListenAndServe(":8080", r)
+}
